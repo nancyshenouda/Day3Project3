26b727193984b8aaed342ebf774c099e4a8978e0
nancyshenouda <Shenouda.nancy@gmail.com>
2021-05-22 12:47:11 -0400

    committing all the work

--- src/test/java/ResetPractice.java
+++ src/test/java/ResetPractice.java
@@ -2,5 +2,6 @@ public class ResetPractice {
     public static void main(String[] args) {
       //work 4 is done
         // work 5 is done after resetting hard
+        // committing all together
     }
 }
